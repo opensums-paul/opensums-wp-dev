cdad9ea4ef723798cbaea1f88239f76490f7ceb9
Paul @opensums <paul@opensums.com>
2020-05-24 18:51:28 +0100

    Strip back settings dead end

--- src/Admin.php
+++ src/Admin.php
@@ -4,29 +4,32 @@ namespace OpenSumsWpDev;
 
 class Admin extends \OpenSumsWp\Admin {
 
-    public function loadAdminMenu() {
+    /**
+     * Add an entry for the plugin to the Admin menu.
+     * 
+     * Invoked as a callback when the admin menu is being created.
+     */
+    public function loadAdminMenu(): void {
         $this->addAdminMenuEntry([
-            'parent' => 'settings',
+            // 'parent' => 'settings',
             'callback' => [$this, 'renderSettingsPage'],
         ]);
     }
 
-    protected function load() {
-        // Add my entry to the admin menu.
-        add_action('admin_menu', [$this, 'loadAdminMenu']);
-        /**
-        * register our wporg_options_page to the admin_menu action hook
-        */
-        add_action( 'admin_menu', 'wporg_options_page');
-    }
-
-    // --- Refactor after here -------------------------------------------------
-
-    /** Step 3. */
-    public function renderSettingsPage() {
+    /**
+     * Show the settings page.
+     * 
+     * Invoked as a callback when the realted admin menu item is selected.
+     */
+    public function renderSettingsPage():void {
         if (!current_user_can('manage_options')) {
             wp_die(__( 'You do not have sufficient permissions to access this page.'));
         }
         $this->plugin->render('admin/settings-page', []);
+    }
+
+    protected function load():void {
+        // Add my entry to the admin menu.
+        add_action('admin_menu', [$this, 'loadAdminMenu']);
     }
 }
